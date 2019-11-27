e1822aa91e23bd65713f6743efe6c10941bd081b
Philip Pearson <ppearson@dsfire.gov.uk>
2019-11-27 16:36:13 +0000

    Simple ML algorithms

--- compare_ml_algorithms.py
+++ compare_ml_algorithms.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn import model_selection
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, BayesianRidge, SGDClassifier, LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -33,7 +34,7 @@ import time
 inputfile = 'W:/2_Reference_Materials/Python/BusSafetyCompliance/Comp.csv'
 y_field = 'Job_Outcome'
 df = pd.read_csv(inputfile)
-df = df.iloc[:, 0:8]
+df = df.iloc[:, 0:3]
 
 # create x and y values
 x = np.array(df.drop(y_field, axis=1))
@@ -52,7 +53,7 @@ models.append(('SGD', SGDClassifier()))
 models.append(('LR', LogisticRegression()))
 models.append(('LDA', LinearDiscriminantAnalysis()))
 models.append(('KNN', KNeighborsClassifier()))
-models.append(('RNC', RadiusNeighborsClassifier(radius = 200)))
+#models.append(('RNC', RadiusNeighborsClassifier(radius = 200)))
 models.append(('DecTree', DecisionTreeClassifier(max_depth=5)))
 models.append(('NB', GaussianNB()))
 #models.append(('CB', ComplementNB())) # Can't have negative X values
@@ -92,25 +93,68 @@ plt.show()
 
 # split dataframe into train and test
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=seed)
-x_min, x_max = x[:, 0].min() - .5, x[:, 0].max() + .5
-y_min, y_max = x[:, 1].min() - .5, x[:, 1].max() + .5
+
+# scale x arrays
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train) 
+x_test_scaled = scaler.transform(x_test) 
+
+x_min, x_max = x_train_scaled[:, 0].min() - .5, x_train_scaled[:, 0].max() + .5
+y_min, y_max = x_train_scaled[:, 1].min() - .5, x_train_scaled[:, 1].max() + .5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
 
 
-figure = plt.figure(figsize=(18, 9))
+figure = plt.figure(figsize=(27, 9))
 i = 1
-#cm = plt.cm.RdBu
+cm = plt.cm.get_cmap('RdBu')
 cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-ax = plt.subplot(len(models), len(models) + 1, i)
+ax = plt.subplot(1, len(models) + 1, i)
 ax.set_title("Input data")
+
 # Plot the training points
-ax.scatter(x_train[:, 0], x_train[:, 1], c=y_train, cmap=cm_bright, edgecolors='k')
+#ax.scatter(x_train_scaled[:, 0], x_train_scaled[:, 1], c=y_train, cmap=cm_bright, edgecolors='k')
+
 # Plot the testing points
-#ax.scatter(x_test[:, 0], x_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,edgecolors='k')
+ax.scatter(x_test_scaled[:, 0], x_test_scaled[:, 1], c=y_test, cmap=cm_bright, alpha=0.3, edgecolors='k')
+
 ax.set_xlim(xx.min(), xx.max())
 ax.set_ylim(yy.min(), yy.max())
 ax.set_xticks(())
 ax.set_yticks(())
-#plt.tight_layout()
+i += 1
+
+# iterate over classifiers
+for name, clf in models:
+    ax = plt.subplot(1, len(models) + 1, i)
+    clf.fit(x_train_scaled, y_train)
+    score = clf.score(x_test_scaled, y_test)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    if hasattr(clf, "decision_function"):
+        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    else:
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+    # Plot the training points
+    #ax.scatter(x_train_scaled[:, 0], x_train_scaled[:, 1], c=y_train, cmap=cm_bright,
+    #            edgecolors='k')
+    # Plot the testing points
+    #ax.scatter(x_test_scaled[:, 0], x_test_scaled[:, 1], c=y_test, cmap=cm_bright,
+    #            edgecolors='k', alpha=0.6)
+
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(name)
+    ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+            size=15, horizontalalignment='right')
+    i += 1
+plt.tight_layout()
 plt.show()
